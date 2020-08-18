d0a0f8f942c61969f79b58167bb33d95b7008af4
hhaaddo <yeahajeong@daum.net>
2020-08-18 16:49:01 +0900

    main 메소드 작성

---
+++ src/main/java/com/yeahajeong/hastagram/HastagramApplication.java
@@ -0,0 +1,11 @@
+package com.yeahajeong.hastagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastagramApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HastagramApplication.class, args);
+    }
+}
