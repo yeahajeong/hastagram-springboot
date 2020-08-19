d45617c64e784310c402c830ef8bb0a722bef77d
hhaaddo <yeahajeong@daum.net>
2020-08-19 15:00:36 +0900

    UserRepository 테스트
    테이블 생성 테스트를 하였다.

---
+++ src/test/java/com/yeahajeong/hastagram/UserRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.yeahajeong.hastagram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Test
+    //테이블 생성 테스트
+    public void test() {
+        //테이블 생성
+    }
+}
