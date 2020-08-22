680b998e4f02d8fe5fdafefcb87e398b65ee0e6e
hhaaddo <yeahajeong@daum.net>
2020-08-22 13:00:41 +0900

    CrudRepository를 JpaRepository로 변경

--- src/main/java/com/yeahajeong/hastagram/repository/UserRepository.java
+++ src/main/java/com/yeahajeong/hastagram/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.yeahajeong.hastagram.repository;
 
 import com.yeahajeong.hastagram.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     //아이디 중복 확인
     User findUserById(String id);
 
-    //이메일 중복확인인
+    //이메일 중복확인
     User findUserByEmail(String email);
-
 }
