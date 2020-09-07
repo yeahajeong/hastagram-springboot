331165aea229bccd884314293773488cd48df5e6
hhaaddo <yeahajeong@daum.net>
2020-09-08 03:22:45 +0900

    SpringBootServletInitializer 상속

--- src/main/java/com/yeahajeong/hastagram/HastagramApplication.java
+++ src/main/java/com/yeahajeong/hastagram/HastagramApplication.java
@@ -2,9 +2,10 @@ package com.yeahajeong.hastagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HastagramApplication {
+public class HastagramApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(HastagramApplication.class, args);
     }
